2e3a6c3673653addc210e3010e5cd57ee781d86c
bingJunior <tariqumair635@gmail.com>
2021-08-18 19:35:43 +0530

    first commit

--- app/src/main/java/com/bingjunior/aboutme/MainActivity.kt
+++ app/src/main/java/com/bingjunior/aboutme/MainActivity.kt
@@ -18,18 +18,25 @@ class MainActivity : AppCompatActivity() {
             addNickname(it)
         }
 
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+        findViewById<TextView>(R.id.name_text).setOnClickListener {
             updateNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-        nicknameTextView.text = editText.text
+        val editText = findViewById<EditText>(R.id.name_edit)
+        val nameTextView = findViewById<TextView>(R.id.name_text)
+        val bioTextView = findViewById<TextView>(R.id.bio_textView)
+        nameTextView.text = editText.text
         editText.visibility = View.GONE
         view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        nameTextView.visibility = View.VISIBLE
+
+        //get name from editText
+        val str:String = editText.text.toString()
+
+        //put it into textView
+        bioTextView.setText("Hi, my name is $str currently, I'm pursuing my bachelor's degree in Information Technology. I'm interested in using technology to solve problems, which is why I’m studying Android Development. My primary goal for this internship is to put everything I've learned so far into practice and to develop new relationships with people who share and differ from my interests. Learn how to solve problems within a specified time frame, as well as how to deal with impending challenges and find solutions within certain deadlines. After eight weeks, all I want out of the internship is to address problems head-on, procrastinate as little as possible, and gain confidence in my abilities.\n")
 
         // Hide the keyboard.
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -37,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateNickname (view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val editText = findViewById<EditText>(R.id.name_edit)
         val doneButton = findViewById<Button>(R.id.done_button)
         editText.visibility = View.VISIBLE
         doneButton.visibility = View.VISIBLE
